495f091b9501b2bd94e5b753a205e6f494561dc7
David Shnayder <dusrdev@gmail.com>
2022-06-11 21:11:47 +0300

    Moved enums to separate file

---
+++ PrettyConsole/Enums.cs
@@ -0,0 +1,14 @@
+﻿ namespace PrettyConsole {
+    /// <summary>
+    /// Provides easy access to the colors which are used throughout this class
+    /// <para>Using this while optionally changing the default colors will make the interface more streamlined</para>
+    /// </summary>
+    public enum Color {
+        Primary,
+        Secondary,
+        Success,
+        Error,
+        Highlight,
+        Input
+    };
+}
