4a9f5093c23f89f092f11bf8466c883f9d12014d
David Shnayder <dusrdev@outlook.com>
2022-11-16 15:46:47 +0200

    Upgraded, removed and optimized

--- PrettyConsole/Extensions.cs
+++ PrettyConsole/Extensions.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace PrettyConsole {
     internal static class Extensions {
-        // Returns the length of the longest string in the collection
-        public static int MaxStringLength(IEnumerable<string> col) => col.Max(s => s.Length);
-
         // Returns a string completed with spaces to fill a certain length
         public static string SuffixWithSpaces(ReadOnlySpan<char> str, int maxLength) {
             if (IsEmptyOrWhiteSpace(str) || str.Length > maxLength) {
@@ -17,19 +14,19 @@ namespace PrettyConsole {
         }
 
         // Checks whether the ReadOnlySpan is empty or only contains whitespace characters
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsEmptyOrWhiteSpace(ReadOnlySpan<char> str) => str.IsEmpty || str.IsWhiteSpace();
 
         // A faster and more memory efficient implementation of string.split
-        public static IEnumerable<string> SplitAsSpan(ReadOnlySpan<char> str, char seperator) {
+        public static IEnumerable<string> Split(string str, char seperator) {
             StringBuilder builder = new();
-            List<string> output = new();
             foreach (var c in str) {
                 if (c.Equals(seperator)) {
                     if (builder.Length <= 0) {
                         continue;
                     }
 
-                    output.Add(builder.ToString().Trim());
+                    yield return builder.ToString().Trim();
                     builder.Clear();
                     continue;
                 }
@@ -37,22 +34,20 @@ namespace PrettyConsole {
                 builder.Append(c);
             }
             if (builder.Length > 0) {
-                output.Add(builder.ToString().Trim());
+                yield return builder.ToString().Trim();
             }
-            return output;
+            yield return string.Empty;
         }
 
         // Returns a user friendly representation
         public static string ToFriendlyString(this TimeSpan elapsed) {
-            string time;
             if (elapsed.TotalSeconds < 60) {
-                time = $"{elapsed.TotalSeconds:0.##}s";
-            } else if (elapsed.Minutes < 60) {
-                time = $"{elapsed.TotalMinutes:0.##}m";
-            } else {
-                time = $"{elapsed.TotalHours:0.###}h";
+                return $"{elapsed.TotalSeconds:0.##}s";
             }
-            return time;
+            if (elapsed.Minutes < 60) {
+                return $"{elapsed.TotalMinutes:0.##}m";
+            }
+            return $"{elapsed.TotalHours:0.###}h";
         }
     }
 }
