bc4d1972fa99477105ed97731dba0f9de5d6c5bc
David Shnayder <dusrdev@outlook.com>
2022-11-26 09:17:35 +0200

    Added a model to contain all elements for a pretty progressbar output

---
+++ PrettyConsole/Models/ProgressBarDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrettyConsole.Models;
+
+/// <summary>
+/// Used as input parameter in an overload of the ProgressBar method.
+/// </summary>
+public readonly ref struct ProgressBarDisplay {
+    /// <summary>
+    /// Header text -> this is where you could write an updatable status message.
+    /// </summary>
+    public readonly string Header { get; init; }
+
+    /// <summary>
+    /// The current progress value.
+    /// </summary>
+    /// <remarks>Recommended to round up to 2 digits</remarks>
+    public required readonly double Percentage { get; init; }
+    
+    /// <summary>
+    /// The color of the header, progress bar bounds and percentage
+    /// </summary>
+    public readonly ConsoleColor Foreground { get; init; }
+
+    /// <summary>
+    /// The color of the filled progress bar
+    /// </summary>
+    public readonly ConsoleColor Progress { get; init; }
+}
