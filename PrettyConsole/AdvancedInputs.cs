c0f5e299049a6a5181ec07d8c2dab07ee0a29f28
David Shnayder <dusrdev@outlook.com>
2023-06-30 20:34:11 +0300

    Updated and added overloads

--- PrettyConsole/AdvancedInputs.cs
+++ PrettyConsole/AdvancedInputs.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics.Contracts;
 
+using PrettyConsole.Models;
+
 using ogConsole = System.Console;
 
 namespace PrettyConsole;
@@ -9,9 +11,10 @@ public static partial class Console {
     /// Used to wait for user input, you can customize <paramref name="message"/> or leave as default
     /// </summary>
     /// <param name="message"><ogConsole>Default value:</ogConsole> "Press any key to continue"</param>
+    [Pure]
     public static void RequestAnyInput(string message = "Press any key to continue") {
         try {
-            Write((message, Colors.Default), ("... ", Colors.Highlight));
+            Write(new TextRenderingScheme((message, Colors.Default), ("... ", Colors.Highlight)));
             ogConsole.ForegroundColor = Colors.Input;
             _ = ogConsole.ReadKey();
             ogConsole.ForegroundColor = Colors.Default;
@@ -30,8 +33,8 @@ public static partial class Console {
     [Pure]
     public static bool Confirm(string message) {
         try {
-            Write((message, Colors.Default), ("? ", Colors.Highlight), ("[", Colors.Default), ("y", Colors.Success),
-                ("/", Colors.Default), ("n", Colors.Error), ("]: ", Colors.Default));
+            Write(new TextRenderingScheme((message, Colors.Default), ("? ", Colors.Highlight), ("[", Colors.Default), ("y", Colors.Success),
+                ("/", Colors.Default), ("n", Colors.Error), ("]: ", Colors.Default)));
             ogConsole.ForegroundColor = Colors.Input;
             var input = ogConsole.ReadLine();
             return string.IsNullOrEmpty(input) || input.ToLower() is "y" or "yes";
--- PrettyConsole/AdvancedOutputs.cs
+++ PrettyConsole/AdvancedOutputs.cs
@@ -1,25 +1,51 @@
+using System.Runtime.CompilerServices;
 using System;
 using System.Threading.Tasks;
 
+using PrettyConsole.Models;
+
 using ogConsole = System.Console;
+using System.Diagnostics.Contracts;
 
 namespace PrettyConsole;
 
 public static partial class Console {
     /// <summary>
-    /// Clears the current line and overrides it with <paramref name="output"/>
+    /// Clears the current line and overrides it with <paramref name="output"/> using the default text color.
     /// </summary>
-    /// <param name="output"></param>
+    /// <param name="output">The string to override the current line with.</param>
+    [Pure]
     public static void OverrideCurrentLine(string output) {
-        OverrideCurrentLine(output, Colors.Default);
+        OverrideCurrentLine(output.AsSpan(), Colors.Default);
     }
 
     /// <summary>
-    /// Clears the current line and overrides it with <paramref name="output"/>
+    /// Clears the current line and overrides it with the specified <paramref name="output"/> using the specified <paramref name="color"/>.
     /// </summary>
-    /// <param name="output"></param>
-    /// <param name="color">foreground color (i.e text color)</param>
+    /// <param name="output">The string to override the current line with.</param>
+    /// <param name="color">The color to use for the overridden line.</param>
+    [Pure]
     public static void OverrideCurrentLine(string output, ConsoleColor color) {
+        OverrideCurrentLine(output.AsSpan(), color);
+    }
+
+    /// <summary>
+    /// Clears the current line and overrides it with <paramref name="buffer"/>
+    /// </summary>
+    /// <param name="buffer"></param>
+    [Pure]
+    public static void OverrideCurrentLine(ReadOnlySpan<char> buffer) {
+        OverrideCurrentLine(buffer, Colors.Default);
+    }
+
+    /// <summary>
+    /// Clears the current line and overrides it with <paramref name="buffer"/>
+    /// </summary>
+    /// <param name="buffer"></param>
+    /// <param name="color">foreground color (i.e text color)</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [Pure]
+    public static void OverrideCurrentLine(ReadOnlySpan<char> buffer, ConsoleColor color) {
         try {
             ogConsole.ResetColor();
             ogConsole.ForegroundColor = color;
@@ -27,7 +53,27 @@ public static partial class Console {
             ogConsole.SetCursorPosition(0, currentLine);
             ogConsole.Write(EmptyLine);
             ogConsole.SetCursorPosition(0, currentLine);
-            ogConsole.Write(output);
+            ogConsole.Out.Write(buffer);
+        } finally {
+            ogConsole.ResetColor();
+        }
+    }
+
+    /// <summary>
+    /// Clears the current line and overrides it with the specified <paramref name="scheme"/>
+    /// </summary>
+    /// <param name="scheme">The <see cref="TextRenderingScheme"/> to render on the current line.</param>
+    [Pure]
+    public static void OverrideCurrentLine(TextRenderingScheme scheme) {
+        try {
+            var currentLine = ogConsole.CursorTop;
+            ogConsole.SetCursorPosition(0, currentLine);
+            ogConsole.Write(EmptyLine);
+            ogConsole.SetCursorPosition(0, currentLine);
+            foreach (var (text, color) in scheme.Segments) {
+                ogConsole.ForegroundColor = color;
+                ogConsole.Write(text);
+            }
         } finally {
             ogConsole.ResetColor();
         }
@@ -37,17 +83,27 @@ public static partial class Console {
     /// Types out the string <paramref name="output"/> character by character with a delay of <paramref name="delay"/> milliseconds between each character.
     /// </summary>
     /// <param name="output"></param>
-    /// <param name="delay">Delay in milliseconds between each character. Default is 50ms.</param>
-    public static async Task TypeWrite(string output, int delay = 50) {
+    /// <param name="delay">Delay in milliseconds between each character. Default is 200ms.</param>
+    [Pure]
+    public static async Task TypeWrite(string output, int delay = 200) {
+        await TypeWrite(output, Colors.Default, delay);
+    }
+
+    /// <summary>
+    /// Types out the string <paramref name="output"/> character by character with a delay of <paramref name="delay"/> milliseconds between each character.
+    /// </summary>
+    /// <param name="output"></param>
+    /// <param name="color">The color in which the output will be displayed</param>
+    /// <param name="delay">Delay in milliseconds between each character. Default is 200ms.</param>
+    [Pure]
+    public static async Task TypeWrite(string output, ConsoleColor color, int delay = 200) {
         try {
             ogConsole.ResetColor();
-            ogConsole.ForegroundColor = Colors.Default;
+            ogConsole.ForegroundColor = color;
             var length = output.Length - 1;
-            int i = 0;
-            while (i < length) {
+            for (int i = 0; i < length; i++) {
                 ogConsole.Write(output[i]);
                 await Task.Delay(delay);
-                i++;
             }
             ogConsole.Write(output[length]);
         } finally {
@@ -59,9 +115,22 @@ public static partial class Console {
     /// Types out the string <paramref name="output"/> character by character with a delay of <paramref name="delay"/> milliseconds between each character and adds a new line at the end.
     /// </summary>
     /// <param name="output"></param>
-    /// <param name="delay">Delay in milliseconds between each character. Default is 50ms.</param>
-    public static async Task TypeWriteLine(string output, int delay = 50) {
+    /// <param name="delay">Delay in milliseconds between each character. Default is 200ms.</param>
+    [Pure]
+    public static async Task TypeWriteLine(string output, int delay = 200) {
         await TypeWrite(output, delay);
+        ogConsole.WriteLine();
+    }
+
+    /// <summary>
+    /// Types out the string <paramref name="output"/> character by character with a delay of <paramref name="delay"/> milliseconds between each character and adds a new line at the end.
+    /// </summary>
+    /// <param name="output"></param>
+    /// <param name="color">The color in which the output will be displayed</param>
+    /// <param name="delay">Delay in milliseconds between each character. Default is 200ms.</param>
+    [Pure]
+    public static async Task TypeWriteLine(string output, ConsoleColor color, int delay = 200) {
+        await TypeWrite(output, color, delay);
         ogConsole.WriteLine();
     }
 }
