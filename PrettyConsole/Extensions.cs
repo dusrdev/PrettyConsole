8428eca592d1b28138054298a0014a729cce0b23
David Shnayder <dusrdev@outlook.com>
2023-06-30 20:34:24 +0300

    Changed all extensions to be internal

--- PrettyConsole/Extensions.cs
+++ PrettyConsole/Extensions.cs
@@ -7,12 +7,12 @@ namespace PrettyConsole;
 
 internal static class Extensions {
     // Returns a string with <paramref name="length"/> spaces
-    public static string GetWhiteSpaces(int length) {
+    internal static string GetWhiteSpaces(int length) {
         return new string(' ', length);
     }
 
     // Returns a string completed with spaces to fill a certain length
-    public static string SuffixWithSpaces(ReadOnlySpan<char> str, int maxLength) {
+    internal static string SuffixWithSpaces(ReadOnlySpan<char> str, int maxLength) {
         if (IsEmptyOrWhiteSpace(str) || str.Length > maxLength) {
             return new string(' ', maxLength);
         }
@@ -21,10 +21,10 @@ internal static class Extensions {
 
     // Checks whether the ReadOnlySpan is empty or only contains whitespace characters
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static bool IsEmptyOrWhiteSpace(ReadOnlySpan<char> str) => str.IsEmpty || str.IsWhiteSpace();
+    internal static bool IsEmptyOrWhiteSpace(ReadOnlySpan<char> str) => str.IsEmpty || str.IsWhiteSpace();
 
     // A faster and more memory efficient implementation of string.split
-    public static IEnumerable<string> Split(string str, char separator) {
+    internal static IEnumerable<string> Split(string str, char separator) {
         StringBuilder builder = new();
         foreach (var c in str) {
             if (c.Equals(separator)) {
@@ -46,7 +46,7 @@ internal static class Extensions {
     }
 
     // Returns a user friendly representation
-    public static string ToFriendlyString(this TimeSpan elapsed) {
+    internal static string ToFriendlyString(this TimeSpan elapsed) {
         if (elapsed.TotalSeconds < 60) {
             return $"{elapsed.TotalSeconds:0.##}s";
         }
