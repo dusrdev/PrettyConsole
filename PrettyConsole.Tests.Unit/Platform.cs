3860b2860b4d85a6aaeddaf292093b6b7bab5278
David Shnayder <dusrdev@gmail.com>
2024-10-04 21:11:43 +0300

    Add unit test to check conditional compilation

---
+++ PrettyConsole.Tests.Unit/Platform.cs
@@ -0,0 +1,20 @@
+using System.Runtime.InteropServices;
+
+namespace PrettyConsole.Tests.Unit;
+
+public class Platform {
+	[Fact]
+	public void CheckConditionalCompilation() {
+		#if Windows
+		string str = "Windows";
+		#else
+		string str = "Unix";
+		#endif
+
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+			str.Should().Be("Windows");
+		} else {
+			str.Should().Be("Unix");
+		}
+	}
+}
