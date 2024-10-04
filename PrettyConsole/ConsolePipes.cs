a6c921e3fb13f2417794142e2f467b698f0034f7
David Shnayder <dusrdev@gmail.com>
2024-10-04 11:05:16 +0300

    Added option to internally override pipes to use in unit testing

---
+++ PrettyConsole/ConsolePipes.cs
@@ -0,0 +1,18 @@
+﻿namespace PrettyConsole;
+
+public static partial class Console {
+	/// <summary>
+	/// The standard input stream.
+	/// </summary>
+	public static TextWriter Out { get; internal set; } = baseConsole.Out;
+
+	/// <summary>
+	/// The error output stream.
+	/// </summary>
+	public static TextWriter Error { get; internal set; } = baseConsole.Error;
+
+	/// <summary>
+	/// The standard input stream.
+	/// </summary>
+	public static TextReader In { get; internal set; } = baseConsole.In;
+}
