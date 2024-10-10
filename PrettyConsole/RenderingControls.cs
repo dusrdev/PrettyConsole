bad5a67fae8aa01609e35f43eb9181552f9368d6
David Shnayder <dusrdev@gmail.com>
2024-10-10 11:43:39 +0300

    Added overloads for ClearNextLines to use Error writer

--- PrettyConsole/RenderingControls.cs
+++ PrettyConsole/RenderingControls.cs
@@ -4,14 +4,33 @@ namespace PrettyConsole;
 
 public static partial class Console {
     /// <summary>
-    /// Clears the next <paramref name="lines"/>
+    /// Clears the next <paramref name="lines"/> (regular output)
     /// </summary>
     /// <param name="lines">Amount of lines to clear</param>
     /// <remarks>
     /// Useful for clearing output of overriding functions, like the ProgressBar
     /// </remarks>
+    public static void ClearNextLines(int lines) => ClearNextLines(lines, Out);
+
+    /// <summary>
+    /// Clears the next <paramref name="lines"/> (regular output)
+    /// </summary>
+    /// <param name="lines">Amount of lines to clear</param>
+    /// <remarks>
+    /// Useful for clearing output of overriding functions, like the ProgressBar
+    /// </remarks>
+    public static void ClearNextLinesError(int lines) => ClearNextLines(lines, Error);
+
+    /// <summary>
+    /// Clears the next <paramref name="lines"/>
+    /// </summary>
+    /// <param name="lines">Amount of lines to clear</param>
+    /// <param name="writer"></param>
+    /// <remarks>
+    /// Useful for clearing output of overriding functions, like the ProgressBar
+    /// </remarks>
     [MethodImpl(MethodImplOptions.Synchronized)]
-    public static void ClearNextLines(int lines) {
+    private static void ClearNextLines(int lines, TextWriter writer) {
         ResetColors();
         using var memoryOwner = Utils.ObtainMemory(baseConsole.BufferWidth);
         Span<char> emptyLine = memoryOwner.Memory.Span.Slice(0, baseConsole.BufferWidth);
@@ -19,7 +38,7 @@ public static partial class Console {
         var currentLine = baseConsole.CursorTop;
         baseConsole.SetCursorPosition(0, currentLine);
         for (int i = 0; i < lines; i++) {
-            Out.WriteLine(emptyLine);
+            writer.WriteLine(emptyLine);
         }
         baseConsole.SetCursorPosition(0, currentLine);
     }
