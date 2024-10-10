11e0f61c88d1c1bb942e65c77fc4beb028e865bb
David Shnayder <dusrdev@gmail.com>
2024-10-10 11:17:27 +0300

    Removed format as Sharpify is now a dependency

--- PrettyConsole/Utils.cs
+++ PrettyConsole/Utils.cs
@@ -7,26 +7,6 @@ namespace PrettyConsole;
 /// </summary>
 public static class Utils {
     /// <summary>
-    /// Returns an elapsed time formatted string, i.e 0.##s or 0.##m or 0.##h
-    /// </summary>
-    /// <param name="elapsed">The elapsed TimeSpan</param>
-    /// <param name="buffer">The buffer to use - ensure sufficient capacity (</param>
-    /// <returns></returns>
-    public static ReadOnlySpan<char> FormatElapsedTime(TimeSpan elapsed, Span<char> buffer) {
-        int bytesWritten;
-        if (elapsed.TotalSeconds < 60) {
-            elapsed.TotalSeconds.TryFormat(buffer, out bytesWritten, "0.##s");
-            return buffer.Slice(0, bytesWritten);
-        }
-        if (elapsed.Minutes < 60) {
-            elapsed.TotalMinutes.TryFormat(buffer, out bytesWritten, "0.##m");
-            return buffer.Slice(0, bytesWritten);
-        }
-        elapsed.TotalHours.TryFormat(buffer, out bytesWritten, "0.##h");
-        return buffer.Slice(0, bytesWritten);
-    }
-
-    /// <summary>
     /// Returns a formatted percentage string, i.e 0,5:##0.##%
     /// </summary>
     /// <param name="percentage"></param>
