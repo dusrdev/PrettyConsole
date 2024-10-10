b8964978bf5477ec0a1a2a5297d272d7bb7dc4da
David Shnayder <dusrdev@gmail.com>
2024-10-10 11:21:30 +0300

    Made utils internal

--- PrettyConsole/Utils.cs
+++ PrettyConsole/Utils.cs
@@ -5,14 +5,14 @@ namespace PrettyConsole;
 /// <summary>
 /// A static class containing utility methods
 /// </summary>
-public static class Utils {
+internal static class Utils {
     /// <summary>
     /// Returns a formatted percentage string, i.e 0,5:##0.##%
     /// </summary>
     /// <param name="percentage"></param>
     /// <param name="buffer"></param>
     /// <returns></returns>
-    public static ReadOnlySpan<char> FormatPercentage(double percentage, Span<char> buffer) {
+    internal static ReadOnlySpan<char> FormatPercentage(double percentage, Span<char> buffer) {
         percentage.TryFormat(buffer, out int bytesWritten, "0,5:##0.##%");
         return buffer.Slice(0, bytesWritten);
     }
@@ -21,5 +21,5 @@ public static class Utils {
     /// Rents a memory owner from the shared memory pool
     /// </summary>
     /// <param name="length">The minimum length</param>
-    public static IMemoryOwner<char> ObtainMemory(int length) => MemoryPool<char>.Shared.Rent(length);
+    internal static IMemoryOwner<char> ObtainMemory(int length) => MemoryPool<char>.Shared.Rent(length);
 }
