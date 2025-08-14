dba44a99dae4f75f35926bcf622e24c50bccebcd
David Shnayder <dusrdev@gmail.com>
2025-08-14 20:09:40 +0300

    Fix current line clearing

--- PrettyConsole/RenderingControls.cs
+++ PrettyConsole/RenderingControls.cs
@@ -15,6 +15,7 @@ public static partial class Console {
         static void InternalClearNextLines(int lines, TextWriter writer) {
             var lineLength = baseConsole.BufferWidth;
             var currentLine = GetCurrentLine();
+            GoToLine(currentLine);
             for (int i = 0; i < lines; i++) {
                 writer.WriteWhiteSpaces(lineLength);
             }
