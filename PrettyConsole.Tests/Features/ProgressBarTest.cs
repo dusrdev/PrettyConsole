8b652a879e213ad5241a4c440ae6377c248c76ab
David Shnayder <dusrdev@gmail.com>
2025-08-15 15:32:37 +0300

    More accurate test scenario

--- PrettyConsole.Tests/Features/ProgressBarTest.cs
+++ PrettyConsole.Tests/Features/ProgressBarTest.cs
@@ -7,15 +7,15 @@ public sealed class ProgressBarTest : IPrettyConsoleTest {
 
     public async ValueTask Implementation() {
         var prg = new ProgressBar {
-            ProgressColor = Color.Yellow,
-            ProgressChar = '»'
+            ProgressColor = Color.Magenta,
+            // ProgressChar = '🧎‍♂️‍➡️'
         };
-        const int count = 1_000;
+        const int count = 333;
         var currentLine = GetCurrentLine();
         for (int i = 1; i <= count; i++) {
             double percentage = 100 * (double)i / count;
-            prg.Update(percentage);
-            await Task.Delay(1);
+            prg.Update(percentage, "TESTING");
+            await Task.Delay(15);
         }
         ClearNextLines(1, OutputPipe.Error);
         GoToLine(currentLine);
