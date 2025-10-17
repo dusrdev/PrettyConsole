dba4c2cb2b1835c9ac391474119aac6bf9095ada
David Shnayder <dusrdev@gmail.com>
2025-10-17 22:22:47 +0300

    Remove discards

--- PrettyConsole/WriteInterpolated.cs
+++ PrettyConsole/WriteInterpolated.cs
@@ -3,14 +3,13 @@ using System.Runtime.CompilerServices;
 namespace PrettyConsole;
 
 public static partial class Console {
+#pragma warning disable IDE0060 // Remove unused parameter
     /// <summary>
     /// Writes interpolated content using <see cref="PrettyConsoleInterpolatedStringHandler"/>.
     /// </summary>
     /// <param name="pipe">Destination pipe. Defaults to <see cref="OutputPipe.Out"/>.</param>
     /// <param name="handler">Interpolated string handler that streams the content.</param>
     public static void WriteInterpolated(OutputPipe pipe, [InterpolatedStringHandlerArgument(nameof(pipe))] PrettyConsoleInterpolatedStringHandler handler = default) {
-        _ = pipe;
-        _ = handler;
     }
 
     /// <summary>
@@ -18,9 +17,10 @@ public static partial class Console {
     /// </summary>
     /// <param name="pipe">Destination pipe. Defaults to <see cref="OutputPipe.Out"/>.</param>
     /// <param name="handler">Interpolated string handler that streams the content.</param>
+
     public static void WriteLineInterpolated(OutputPipe pipe, [InterpolatedStringHandlerArgument(nameof(pipe))] PrettyConsoleInterpolatedStringHandler handler = default) {
-        _ = pipe;
-        _ = handler;
         NewLine(pipe);
     }
+#pragma warning restore IDE0060 // Remove unused parameter
+
 }
