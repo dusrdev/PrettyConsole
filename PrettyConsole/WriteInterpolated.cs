bd846ddd249ae4c679b068b55cac33db6292efc3
David Shnayder <dusrdev@gmail.com>
2025-10-18 17:07:55 +0300

    Remove interpolated suffix

--- PrettyConsole/WriteInterpolated.cs
+++ PrettyConsole/WriteInterpolated.cs
@@ -5,11 +5,29 @@ namespace PrettyConsole;
 public static partial class Console {
 #pragma warning disable IDE0060 // Remove unused parameter
     /// <summary>
+    /// Writes interpolated content using <see cref="PrettyConsoleInterpolatedStringHandler"/> to <see cref="OutputPipe.Out"/>.
+    /// </summary>
+    /// <param name="handler">Interpolated string handler that streams the content.</param>
+    public static void Write([InterpolatedStringHandlerArgument] PrettyConsoleInterpolatedStringHandler handler = default) {
+        ResetColors();
+	}
+
+    /// <summary>
     /// Writes interpolated content using <see cref="PrettyConsoleInterpolatedStringHandler"/>.
     /// </summary>
     /// <param name="pipe">Destination pipe. Defaults to <see cref="OutputPipe.Out"/>.</param>
     /// <param name="handler">Interpolated string handler that streams the content.</param>
-    public static void WriteInterpolated(OutputPipe pipe, [InterpolatedStringHandlerArgument(nameof(pipe))] PrettyConsoleInterpolatedStringHandler handler = default) {
+    public static void Write(OutputPipe pipe, [InterpolatedStringHandlerArgument(nameof(pipe))] PrettyConsoleInterpolatedStringHandler handler = default) {
+        ResetColors();
+    }
+
+    /// <summary>
+    /// Writes interpolated content using <see cref="PrettyConsoleInterpolatedStringHandler"/> to <see cref="OutputPipe.Out"/>.
+    /// </summary>
+    /// <param name="handler">Interpolated string handler that streams the content.</param>
+    public static void WriteLine([InterpolatedStringHandlerArgument] PrettyConsoleInterpolatedStringHandler handler = default) {
+        ResetColors();
+        NewLine(OutputPipe.Out);
     }
 
     /// <summary>
@@ -17,10 +35,9 @@ public static partial class Console {
     /// </summary>
     /// <param name="pipe">Destination pipe. Defaults to <see cref="OutputPipe.Out"/>.</param>
     /// <param name="handler">Interpolated string handler that streams the content.</param>
-
-    public static void WriteLineInterpolated(OutputPipe pipe, [InterpolatedStringHandlerArgument(nameof(pipe))] PrettyConsoleInterpolatedStringHandler handler = default) {
+    public static void WriteLine(OutputPipe pipe, [InterpolatedStringHandlerArgument(nameof(pipe))] PrettyConsoleInterpolatedStringHandler handler = default) {
+        ResetColors();
         NewLine(pipe);
     }
 #pragma warning restore IDE0060 // Remove unused parameter
-
 }
