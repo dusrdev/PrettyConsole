fcdb93271bc50d0466a86a0faed05fa7b7e6f38f
David Shnayder <dusrdev@gmail.com>
2025-10-25 19:29:07 +0300

    Improved writewhitespaces and moved to extensions

---
+++ PrettyConsole/Extensions.cs
@@ -0,0 +1,20 @@
+namespace PrettyConsole;
+
+internal static class Extensions {
+    private static readonly string WhiteSpaces = new(' ', 256);
+
+    /// <summary>
+    /// Writes whitespace to a <see cref="TextWriter"/> up to length by chucks
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="length"></param>
+    internal static void WriteWhiteSpaces(this TextWriter writer, int length) {
+        ReadOnlySpan<char> whiteSpaces = WhiteSpaces;
+
+        while (length > 0) {
+            int cur_length = Math.Min(length, 256);
+            writer.Write(whiteSpaces.Slice(0, cur_length));
+            length -= cur_length;
+		}
+    }
+}
