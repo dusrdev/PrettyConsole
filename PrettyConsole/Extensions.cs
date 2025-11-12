9032fd77837e9d30946bb7763506e1acf5d81367
David Shnayder <dusrdev@gmail.com>
2025-11-12 21:08:01 +0200

    Use extension members

--- PrettyConsole/Extensions.cs
+++ PrettyConsole/Extensions.cs
@@ -1,20 +1,22 @@
 namespace PrettyConsole;
 
 internal static class Extensions {
-    private static readonly string WhiteSpaces = new(' ', 256);
+    extension(TextWriter @this) {
+        /// <summary>
+        /// Writes whitespace to this <see cref="TextWriter"/> up to length by chucks
+        /// </summary>
+        /// <param name="length"></param>
+        internal void WriteWhiteSpaces(int length) {
+            ReadOnlySpan<char> whiteSpaces = WhiteSpaces;
 
-    /// <summary>
-    /// Writes whitespace to a <see cref="TextWriter"/> up to length by chucks
-    /// </summary>
-    /// <param name="writer"></param>
-    /// <param name="length"></param>
-    internal static void WriteWhiteSpaces(this TextWriter writer, int length) {
-        ReadOnlySpan<char> whiteSpaces = WhiteSpaces;
-
-        while (length > 0) {
-            int cur_length = Math.Min(length, 256);
-            writer.Write(whiteSpaces.Slice(0, cur_length));
-            length -= cur_length;
+            while (length > 0) {
+                int cur_length = Math.Min(length, 256);
+                @this.Write(whiteSpaces.Slice(0, cur_length));
+                length -= cur_length;
+            }
         }
     }
+
+
+    private static readonly string WhiteSpaces = new(' ', 256);
 }
