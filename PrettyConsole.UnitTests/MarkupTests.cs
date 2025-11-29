aaa1a8c30fc55ef99b98fbba6a5ffae59bc882f0
David Shnayder <dusrdev@gmail.com>
2025-11-29 14:49:24 +0200

    Cover all constants in markup

--- PrettyConsole.UnitTests/MarkupTests.cs
+++ PrettyConsole.UnitTests/MarkupTests.cs
@@ -2,8 +2,16 @@ namespace PrettyConsole.UnitTests;
 
 public class MarkupTests {
     [Test]
-    public async Task Markup_Enabled_MatchesConsoleRedirectionState() {
-        await Assert.That(Markup.Underline.Equals("\e[4m", StringComparison.Ordinal)).IsTrue();
-        await Assert.That(Markup.Reset.Equals("\e[0m", StringComparison.Ordinal)).IsTrue();
+    [Arguments(Markup.Reset, "\e[0m")]
+    [Arguments(Markup.Underline, "\e[4m")]
+    [Arguments(Markup.ResetUnderline, "\e[24m")]
+    [Arguments(Markup.Bold, "\e[1m")]
+    [Arguments(Markup.ResetBold, "\e[22m")]
+    [Arguments(Markup.Italic, "\e[3m")]
+    [Arguments(Markup.ResetItalic, "\e[23m")]
+    [Arguments(Markup.Strikethrough, "\e[9m")]
+    [Arguments(Markup.ResetStrikethrough, "\e[29m")]
+    public async Task Markup_Constants(string actual, string expected) {
+        await Assert.That(actual).IsEqualTo(expected).IgnoringCase();
     }
 }
